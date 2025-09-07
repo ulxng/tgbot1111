6e1d69798468d31ae5ec838c92b75cc3fdabc137
Anri27 <star.platinum.00.00@mail.ru>
2025-09-07 13:57:33 +0300

    first commit

---
+++ tg.py
@@ -0,0 +1,10 @@
+import telebot;
+bot = telebot.TeleBot('7753353919:AAGEomI4uCCdRVm1dxQ0nxTpViQvLVgxWh4')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.upper() == "1":
+        bot.send_message(message.from_user.id, "2")
+    else:
+        pass
+
+bot.polling(none_stop=True, interval=0)
