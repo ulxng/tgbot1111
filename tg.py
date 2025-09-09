45a33886cd430c0e4ec4f4f9a906db81bb6558eb
Anri27 <star.platinum.00.00@mail.ru>
2025-09-09 15:43:26 +0300

    add commands

--- tg.py
+++ tg.py
@@ -3,12 +3,9 @@ import telebot
 bot = telebot.TeleBot("7753353919:AAGEomI4uCCdRVm1dxQ0nxTpViQvLVgxWh4")
 
 
-@bot.message_handler(content_types=["text"])
-def get_text_messages(message):
-    if message.text.upper() == "1":
-        bot.send_message(message.from_user.id, "2")
-    else:
-        pass
+@bot.message_handler(commands=["start", "help"])
+def send_message(message):
+    bot.reply_to(message="Hello world!")
 
 
 bot.polling(none_stop=True, interval=0)
