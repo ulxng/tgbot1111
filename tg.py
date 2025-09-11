6997084a085f7afc252b642f9f278fd5ee8b2585
Anri27 <star.platinum.00.00@mail.ru>
2025-09-12 01:38:40 +0300

    add send_ban_message function

--- tg.py
+++ tg.py
@@ -10,9 +10,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-phrases = {"помощь где?", "хелп ми", "мне нужна помощь", "i need help", "помощь", "хелп", "help"}
-
-
 @bot.message_handler(commands=["start"])
 def send_start_message(message):
     bot.send_message(
@@ -34,31 +31,11 @@ def send_me_message(message):
     print(f"Обработана команда /me от пользователя ID: {message.from_user.id}")
 
 
-@bot.message_handler(func=lambda message: message.text.strip() in phrases)
-def send_help1_message(message):
-    check = message.text.strip()
+@bot.message_handler(content_types="text")
+def send_ban_message(message):
+    if not message.text.strip().lower().find("заработок"):
+        bot.reply_to(message, "Этобан")
 
-    if check == "хелп ми":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "помощь где?":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "мне нужна помощь":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "I need help":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "help":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "хелп":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
-    elif check == "помощь":
-        bot.reply_to(message, "Тебе тут никто не поможет.")
-        print(f"Обработана команда /help1 от пользователя ID: {message.from_user.id}")
 
 def main():
     bot.polling(none_stop=True, interval=1)
